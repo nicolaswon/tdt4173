0a4f32101ca33c3cd6948664966c2b109736b122
Nicolas Wong <nicolas@spond.com>
2022-11-05 14:18:45 +0100

    added distance to store

---
+++ src/feature_engineering/sklearn_transformers.py
@@ -0,0 +1,144 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import rbf_kernel
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import FunctionTransformer, OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
+from feature_engineering.store_features import *
+
+def is_name(function_transformer, feature_names_in):
+    return ["is"]  # feature names out
+
+def is_null(x):
+    return x.notna()
+
+def is_null_pipeline():
+    return make_pipeline(
+        FunctionTransformer(is_null, feature_names_out=is_name)
+        )
+    
+def one_hot_encode_pipeline():
+    return make_pipeline(
+        OneHotEncoder(handle_unknown="ignore", sparse=False)
+    )
+    
+def column_ratio(X):
+    return X[:, [0]] / X[:, [1]]
+
+def ratio_name(function_transformer, feature_names_in):
+    return ["ratio"]  # feature names out
+
+def ratio_pipeline():
+    return make_pipeline(
+        SimpleImputer(strategy="median"),
+        FunctionTransformer(column_ratio, feature_names_out=ratio_name),
+        StandardScaler()
+    )
+
+class ClusterSimilarity(BaseEstimator, TransformerMixin):
+    def __init__(self, n_clusters=10, gamma=1.0, random_state=None, sample_weight=None):
+        self.n_clusters = n_clusters
+        self.gamma = gamma
+        self.random_state = random_state
+        self.sample_weight = sample_weight
+        
+    def fit(self, X, y=None):
+        self.kmeans_ = KMeans(self.n_clusters, random_state=self.random_state)
+        self.kmeans_.fit(X, sample_weight=self.sample_weight['revenue'])
+        return self
+    
+    def transform(self, X):
+        return rbf_kernel(X, self.kmeans_.cluster_centers_, gamma=self.gamma)
+    
+    def get_feature_names_out(self, names=None):
+        return [f'Cluster {i} similarity' for i in range(self.n_clusters)]
+    
+    
+class StoresInRadiusTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, radius, store_type_group=None, stores_extra=None):
+        self.radius = radius
+        self.store_type_group = store_type_group
+        self.stores_extra = stores_extra
+        
+    def fit(self, X, y=None):
+        if self.stores_extra is not None:
+            self.geo_map_ = pd.concat([X, self.stores_extra], ignore_index=True).drop_duplicates()
+        else:
+            self.geo_map_ = X
+            
+        return self
+    
+    def transform(self, X, y=None):
+        X = X.copy()
+        df = stores_in_radius(X, self.geo_map_, radius=self.radius, store_type_group=self.store_type_group)
+        return pd.DataFrame(X.merge(df, on="store_id", how="left")[f'count'])
+    
+    def get_feature_names_out(self, names=None):
+        return ['in_radius']
+    
+    
+class AggTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, agg_cols, agg_name, calculations, stores_extra = None, sample_revenue=None):
+        self.agg_cols = agg_cols
+        self.agg_name = agg_name
+        self.calculations = calculations
+        self.sample_revenue = sample_revenue
+        self.stores_extra = stores_extra
+        
+        
+    def fit(self, X, y=None):
+        X = X.copy()
+        
+        if self.stores_extra is not None:
+            X = pd.concat([X, self.stores_extra], ignore_index=True).drop_duplicates()
+        
+        if self.sample_revenue is not None:
+            X['revenue'] = self.sample_revenue['revenue']
+        
+        mapping = self.calculations(X, self.agg_cols, self.agg_name)
+        self.mapping_ = mapping
+        
+        return self
+    
+    def transform(self, X, y=None):
+        X = X.copy()
+        
+        for index, row in self.mapping_.iterrows():
+            ind = (X[self.agg_cols] == row[self.agg_cols]).all(axis=1)
+            X.loc[ind, self.agg_name] = row[self.agg_name]
+            
+        return pd.DataFrame(X[self.agg_name])
+    
+    def get_feature_names_out(self, names=None):
+        return [self.agg_name]
+    
+
+class ClosestStore(BaseEstimator, TransformerMixin):
+    def __init__(self, stores_extra=None, store_type_groups=['lv1_desc']):
+        self.stores = None
+        self.store_type_groups = store_type_groups
+        self.stores_extra = stores_extra
+        
+    def fit(self, X, y=None):
+        X = X.copy()[['store_id', 'lat', 'lon']+self.store_type_groups]
+        if self.stores_extra is not None:
+            self.stores = pd.concat([X, self.stores_extra[['store_id', 'lat', 'lon']+self.store_type_groups]], ignore_index=True)
+        else:
+            self.stores = X
+            
+        return self
+    
+    def transform(self, X, y=None):
+        X = X.copy()[['store_id', 'lat', 'lon']+self.store_type_groups]
+        
+        combined_df = pd.concat([X, self.stores], ignore_index=True)
+        
+        self.closest = store_closest_by_store_groups(X, combined_df, self.store_type_groups)
+        
+        return X.merge(self.closest, on="store_id", how="left").iloc[:,-len(self.store_type_groups):]
+    
+    def get_feature_names_out(self, names=None):
+        return [f"distance_{store_type}" for store_type in self.store_type_groups]
--- src/feature_engineering/store_features.py
+++ src/feature_engineering/store_features.py
@@ -1,9 +1,5 @@
 import pandas as pd
-from sklearn.base import BaseEstimator, TransformerMixin
 from scipy.spatial.distance import cdist
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import FunctionTransformer, OneHotEncoder, StandardScaler
-from sklearn.impute import SimpleImputer
 
 def store_count(X, agg_cols, agg_name, **kwargs):
     return X.groupby(agg_cols).size().reset_index(name=agg_name, drop=False)
@@ -14,72 +10,6 @@ def average_revenue(X, agg_cols, agg_name, **kwargs):
 def total_revenue(X, agg_cols, agg_name, **kwargs):
     return  X.groupby(agg_cols)['revenue'].agg('sum').reset_index(name=agg_name, drop=False)
 
-    
-
-def is_name(function_transformer, feature_names_in):
-    return ["is"]  # feature names out
-
-def is_null(x):
-    return x.notna()
-
-def is_null_pipeline():
-    return make_pipeline(
-        FunctionTransformer(is_null, feature_names_out=is_name)
-        )
-    
-def one_hot_encode_pipeline():
-    return make_pipeline(
-        OneHotEncoder(handle_unknown="ignore", sparse=False)
-    )
-    
-def column_ratio(X):
-    return X[:, [0]] / X[:, [1]]
-
-def ratio_name(function_transformer, feature_names_in):
-    return ["ratio"]  # feature names out
-
-def ratio_pipeline():
-    return make_pipeline(
-        SimpleImputer(strategy="median"),
-        FunctionTransformer(column_ratio, feature_names_out=ratio_name),
-        StandardScaler()
-    )
-
-class AggTransformer(BaseEstimator, TransformerMixin):
-    def __init__(self, agg_cols, agg_name, calculations, stores_extra = None, sample_revenue=None):
-        self.agg_cols = agg_cols
-        self.agg_name = agg_name
-        self.calculations = calculations
-        self.sample_revenue = sample_revenue
-        self.stores_extra = stores_extra
-        
-        
-    def fit(self, X, y=None):
-        X = X.copy()
-        
-        if self.stores_extra is not None:
-            X = pd.concat([X, self.stores_extra], ignore_index=True).drop_duplicates()
-        
-        if self.sample_revenue is not None:
-            X['revenue'] = self.sample_revenue['revenue']
-        
-        mapping = self.calculations(X, self.agg_cols, self.agg_name)
-        self.mapping_ = mapping
-        
-        return self
-    
-    def transform(self, X, y=None):
-        X = X.copy()
-        
-        for index, row in self.mapping_.iterrows():
-            ind = (X[self.agg_cols] == row[self.agg_cols]).all(axis=1)
-            X.loc[ind, self.agg_name] = row[self.agg_name]
-            
-        return pd.DataFrame(X[self.agg_name])
-    
-    def get_feature_names_out(self, names=None):
-        return [self.agg_name]
-    
 def stores_in_radius(stores_df, compare_df, radius=0.1, store_type_group=None):
     mat = cdist(stores_df[['lat', 'lon']],
                 compare_df[['lat', 'lon']], metric="euclidean")
@@ -100,9 +30,6 @@ def stores_in_radius(stores_df, compare_df, radius=0.1, store_type_group=None):
             nearby_stores = row.dropna().index.values
             index_type = compare_df.loc[compare_df['store_id'] == index, store_type_group].iat[0]
             
-            # compare_df[compare_df['store_id']
-            #                         == index][store_type_group].values[0]
-            
             number_same = compare_df[(compare_df['store_id'].isin(nearby_stores)) & (
                 compare_df[store_type_group] == index_type)]['store_id'].count()
             
@@ -111,25 +38,43 @@ def stores_in_radius(stores_df, compare_df, radius=0.1, store_type_group=None):
         df = pd.DataFrame.from_dict(store_count, orient='index', columns=["count"])
         df.index.rename('store_id', inplace=True)
         return df.reset_index()
+
+def store_closest(stores_df, compare_df, store_type_group="lv4_desc"):
+    """
+    Id and distance of the closest store of same type in the same group.
+    """
     
-class StoresInRadiusTransformer(BaseEstimator, TransformerMixin):
-    def __init__(self, radius, store_type_group=None, stores_extra=None):
-        self.radius = radius
-        self.store_type_group = store_type_group
-        self.stores_extra = stores_extra
+    store_types_in_group = stores_df[store_type_group].unique()
+    df_list = []
+    for store_type in store_types_in_group:
+        stores_by_type = stores_df[stores_df[store_type_group] == store_type]
+        stores_comp_by_type = compare_df[compare_df[store_type_group] == store_type]
         
-    def fit(self, X, y=None):
-        if self.stores_extra is not None:
-            self.geo_map_ = pd.concat([X, self.stores_extra], ignore_index=True).drop_duplicates()
-        else:
-            self.geo_map_ = X
-            
-        return self
+        mat = cdist(stores_by_type[['lat', 'lon']], stores_comp_by_type[['lat', 'lon']], metric='euclidean')
+        
+        df = pd.DataFrame(
+            mat, index=stores_by_type['store_id'], columns=stores_comp_by_type['store_id'])
+        
+        df = df[df > 0]
+        
+        stores = df.index
+        closest = df.idxmin(axis=1)
+        distance = df.min(axis=1)
+        
+        new_df = pd.DataFrame({'store_id': stores.values, 'closest_store': closest.values, 'distance': distance.values})
+        df_list.append(new_df)
+        
     
-    def transform(self, X, y=None):
-        X = X.copy()
-        df = stores_in_radius(X, self.geo_map_, radius=self.radius, store_type_group=self.store_type_group)
-        return pd.DataFrame(X.merge(df, on="store_id", how="left")[f'count'])
+    return pd.concat(df_list, ignore_index=True)
+
+
+def store_closest_by_store_groups(stores_df, compare_df, store_type_groups):
+    df_list = []
     
-    def get_feature_names_out(self, names=None):
-        return ['in_radius']
+    for store_type_group in store_type_groups:
+        df = store_closest(stores_df, compare_df, store_type_group=store_type_group)
+        df.rename(columns={'distance': f'distance_to_{store_type_group}'}, inplace=True)
+        df_list.append(df[['store_id', f'distance_to_{store_type_group}']])
+
+    dfs = [df.set_index('store_id') for df in df_list]
+    return pd.concat(dfs, axis=1).reset_index()
